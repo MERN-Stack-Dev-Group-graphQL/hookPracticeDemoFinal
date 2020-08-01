04896ab219adce165bd535635697ce368aef2660
Jizong Liang <Liang_Jizong@comcast.com>
2020-08-01 16:59:03 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 import { myFunction } from "./myFunction";
+import { useAsync } from "./useAsync";
 
 function App() {
+  const { execute, pending, value, error } = useAsync(myFunction, false);
   return <div className="App"></div>;
 }
 
